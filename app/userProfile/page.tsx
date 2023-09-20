a471f695ff0da5ee8b0f713eaf85cef551573987
UmAngJR <umangrathod190@gmail.com>
2023-09-21 00:42:25 +0530

    changes

--- app/userProfile/page.tsx
+++ app/userProfile/page.tsx
@@ -1,7 +1,6 @@
-"use client";
-
-import React, { useEffect, useState } from "react";
-import { ProfilePropsWithFriend } from "@/types";
+"use client"
+import React from "react";
+import { ProfilePropsWithFriend, userDetails } from "@/types";
 import Image from "next/image";
 
 const UserProfile: React.FC<ProfilePropsWithFriend> = ({
@@ -12,58 +11,26 @@ const UserProfile: React.FC<ProfilePropsWithFriend> = ({
   imageWidth,
   imageHeight,
 }) => {
-  const [coverStyle, setCoverStyle] = useState<React.CSSProperties>({});
-
-  useEffect(() => {
-    const parentContainer = document.getElementById("parent-container");
-    if (parentContainer) {
-      const parentHeight = parentContainer.clientHeight;
-      setCoverStyle({
-        width: "100%",
-        height: "100%",
-        position: "absolute",
-        top: 0,
-        left: 0,
-        objectFit: "cover",
-      });
-    }
-  }, []);
-
   return (
-    <div
-      id="parent-container"
-      className="relative overflow-hidden"
-      style={{ height: "300px", maxWidth: "100%" }}
-    >
+    <div className="relative overflow-hidden">
       <Image
         src="/cover-image.jpg"
         alt="Cover Image"
-        objectFit="cover"
         width={1820}
         height={300}
-        style={coverStyle}
+        layout="responsive"
+        objectFit="cover"
+        className="w-full h-auto"
       />
-      <div
-        className="absolute bottom-0 left-0 p-4 bg-black bg-opacity-50 flex items-center"
-        style={{
-          width: "100%",
-          position: "absolute",
-          bottom: "-10px",
-          left: "50px",
-          zIndex: "999"
-        }}
-      >
-        <div
-          className={`w-${imageWidth} h-${imageHeight} rounded-full overflow-hidden`}
-          onClick={onClick}
-          style={{ zIndex: 999 }}
-        >
+      <div className="absolute bottom-0 left-0 p-4 bg-black bg-opacity-50 flex items-center w-full">
+        <div className={`w-${imageWidth} h-${imageHeight} rounded-full overflow-hidden`} onClick={onClick}>
           <Image
             src="/user-profile.png"
             alt="User Profile"
             width={100}
             height={100}
-            className="overflow-hidden"
+            objectFit="cover"
+            className="w-full h-full rounded-full"
           />
         </div>
         <div className="ml-2">
